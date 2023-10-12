0da01bff6036e16d239a87b80f09843f27cef36b
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-10-12 11:08:44 +0300

    initialization of spring project

---
+++ Back-End (Java-Spring)/DerpinesCards/src/main/java/com/example/DerpinesCards/DerpinesCardsApplication.java
@@ -0,0 +1,13 @@
+package com.example.DerpinesCards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerpinesCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerpinesCardsApplication.class, args);
+	}
+
+}
