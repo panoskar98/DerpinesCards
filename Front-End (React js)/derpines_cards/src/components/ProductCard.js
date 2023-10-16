5db7332d0e489346ec1ed185b985edb0b0be07c9
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-10-16 09:42:07 +0300

    changed card elevation

--- Front-End (React js)/derpines_cards/src/components/ProductCard.js
+++ Front-End (React js)/derpines_cards/src/components/ProductCard.js
@@ -5,7 +5,7 @@ import customTheme from "../Themes/CustomTheme";
 const ProductCard = () => {
     return(
         <Grid item xs={12} sm={6} md={4} lg={3} sx={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-            <Paper elevation={3} sx={{width: "250px", borderRadius: "10px"}}>
+            <Paper elevation={8} sx={{width: "250px", borderRadius: "10px"}}>
                 <img style={{width: "100%", height: "12rem"}} src={logo} alt="product" />
                 <Box padding={"4px"} color={customTheme.palette.dark2.main} justifyContent={"center"} sx={{backgroundColor: customTheme.palette.secondary.main, borderTopLeftRadius: "10px", borderTopRightRadius: "10px"}}>
                     <Typography variant="subtitle1" component="h2" align="center">
