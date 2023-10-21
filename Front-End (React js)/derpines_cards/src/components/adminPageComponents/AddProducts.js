f17d5043fb3ac13294e802b9ed0a53a486736806
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-10-21 11:04:44 +0300

    added product Add form

--- Front-End (React js)/derpines_cards/src/components/adminPageComponents/AddProducts.js
+++ Front-End (React js)/derpines_cards/src/components/adminPageComponents/AddProducts.js
@@ -1,23 +1,21 @@
-import { Box, Container, FormControl, Grid, InputLabel, MenuItem, Select, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, FormControl, FormHelperText, Grid, InputAdornment, InputLabel, MenuItem, OutlinedInput, Select, TextField, Typography } from "@mui/material";
 import { useState } from "react";
 
 const AddProducts = () => {
     const [type, setType] = useState("card")
     const [title, setTitle] = useState("title")
-    const [description,setDescription] = useState("description")
+    const [description, setDescription] = useState("description")
+    const [color, setColor] = useState("Color")
+    const [weight, setWeight] = useState(0)
+    const [price, setPrice] = useState(0)
+    const [imgUrl, setImgUrl] = useState("")
 
-    const handleTitle = (event) => {
-        setTitle(event.target.value)
-    }
 
-    const handleDescription = (event) => {
-        setDescription(event.target.value)
-    }
     return (
         <Container>
             <Grid container justifyContent="center" spacing={5}>
                 <Grid item sm={12}>
-                    <Typography variant="h4" component="h1" display="flex" justifyContent="center" width="100%" padding="5px" marginBottom="20px">
+                    <Typography variant="h4" component="h1" display="flex" justifyContent="center" width="100%" padding="5px">
                         Add Product
                     </Typography>
                 </Grid>
@@ -43,7 +41,9 @@ const AddProducts = () => {
                         required
                         id="outlined-required"
                         label="Title"
-                        onChange={handleTitle}
+                        onChange={(event) => {
+                            setTitle(event.target.value)
+                        }}
                     />
                 </Grid>
                 <Grid item sm={12} display="flex" justifyContent="center">
@@ -52,18 +52,50 @@ const AddProducts = () => {
                         label="Description"
                         multiline
                         rows={3}
-                        onChange={handleDescription}
+                        onChange={(event) => { setDescription(event.target.value) }}
+                    />
+                </Grid>
+                <Grid item sm={12} display="flex" justifyContent="center">
+                    <TextField
+                        id="outlined"
+                        label="Color"
+                        onChange={(event) => { setColor(event.target.value) }}
+                    />
+                </Grid>
+                <Grid item sm={12} display="flex" justifyContent="center">
+                    <TextField
+                        label="Weight"
+                        id="outlined-start-adornment"
+                        sx={{ m: 1, width: '25ch' }}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">gr</InputAdornment>,
+                        }}
+                        onChange={(event) => { setWeight(event.target.value) }}
                     />
                 </Grid>
                 <Grid item sm={12} display="flex" justifyContent="center">
                     <TextField
                         required
-                        id="outlined-required"
-                        label="color"
-                        onChange={handleTitle}
+                        label="price"
+                        id="outlined-start-adornment"
+                        sx={{ m: 1, width: '25ch' }}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">€</InputAdornment>,
+                        }}
+                        onChange={(event) => { setPrice(event.target.value) }}
                     />
                 </Grid>
-
+                <Grid item sm={12} display="flex" justifyContent="center">
+                    <TextField
+                        id="outlined"
+                        label="Image URL"
+                        onChange={(event) => { setImgUrl(event.target.value) }}
+                        multiline
+                    />
+                </Grid>
+                <Grid item sm={12} display="flex" justifyContent="center">
+                    <Button variant="contained">Submit</Button>
+                </Grid>
             </Grid>
         </Container>
     );
