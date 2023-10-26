5da25a4420960d20a2d0e1f2d883063c927ce678
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-10-26 16:18:49 +0300

    fixed ignored property in order that didnt output customer

--- Back-End (Java-Spring)/DerpinesCards/src/main/java/com/example/DerpinesCards/models/Order.java
+++ Back-End (Java-Spring)/DerpinesCards/src/main/java/com/example/DerpinesCards/models/Order.java
@@ -22,7 +22,7 @@ public class Order {
     @Column(name = "completed")
     private boolean completed;
 
-    @JsonIgnore
+    @JsonIgnoreProperties({"customerOrders"})
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "customer_id")
     private Customer customer;
