04a8db3b3372837f7fd8529fc6d750e93f37c279
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-10-27 20:02:53 +0300

    general changes

--- Front-End (React js)/derpines_cards/src/components/adminPageComponents/OrderPreview.js
+++ Front-End (React js)/derpines_cards/src/components/adminPageComponents/OrderPreview.js
@@ -1,22 +1,38 @@
-import { Avatar, Box, Button, Divider, Grid, List, ListItem, ListItemIcon, ListItemText, ListSubheader, Typography } from "@mui/material";
-import { useLocation, useParams } from "react-router-dom";
+import { Avatar, Box, Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Divider, Grid, List, ListItem, ListItemIcon, ListItemText, ListSubheader, Typography } from "@mui/material";
+import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import ParseComments from "../../generalFunctions/ParseComments";
+import axios from "axios";
+import { useState } from "react";
 
 function OrderPreview() {
     const location = useLocation()
     const params = useParams()
+    const navigate = useNavigate()
+    const [deleteDialog, setDeleteDialog] = useState(false)
 
     const order = location.state.orders.find(order => order.orderId === Number(params.id));
 
+    const completion = order.completed
+
     const commentsArray = ParseComments(order.customer.comments)
-    console.log(commentsArray)
+    console.log(completion)
+    
+    const setOrderCompletion = () => {
+        axios.put(`http://localhost:4000/orders/${params.id}`, Boolean(!completion), {headers: {'Content-Type': 'application/json'}}).then(alert("order completion changed")).then(navigate("/admin/orders"))
+    }
+
+    const deleteOrder = () => {
+        axios.delete(`http://localhost:4000/orders/${params.id}`).then(alert("Order Deleted"))
+    }
 
     return (
         <Grid container gap={2} display="flex" justifyContent="center">
             <Grid item sm={12} display="flex" justifyContent="center" gap={2}>
+                <Link to={'/admin/orders'}>
                 <Button variant="contained" >Back</Button>
-                <Button variant="contained">Complete Order</Button>
-                <Button variant="contained" >Delete Order</Button>
+                </Link>
+                <Button variant="contained" onClick={setOrderCompletion}>{order.completed ? "Order Completed" : "Order Incomplete"}</Button>
+                <Button variant="contained" onClick={() => setDeleteDialog(true)}>Delete Order</Button>
             </Grid>
             <Grid item sm={12} md={5}>
                 <Box className="glass" padding={2} height="auto">
@@ -96,7 +112,30 @@ function OrderPreview() {
                     })}
                 </Box>
             </Grid>
+            <Dialog
+                open={deleteDialog}
+                onClose={() => setDeleteDialog(false)}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+                hideBackdrop={true}
+            >
+                <DialogTitle id="alert-dialog-title">
+                    {"Are you sure you want to delete this product?"}
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText id="alert-dialog-description">
+                        Deleting is irreversable
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button color="dark2" onClick={() => setDeleteDialog(false)}>Go Back</Button>
+                    <Button color="dark2" onClick={deleteOrder} autoFocus>
+                        Delete
+                    </Button>
+                </DialogActions>
+            </Dialog>
         </Grid>
+        
     )
 }
 
