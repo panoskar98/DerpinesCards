d1faf49999f31afdd1cc9a1872e3557cf25253a5
Panagiotis Karapiperis <panagiotis_karapiperis@hotmail.com>
2023-10-31 08:57:45 +0200

    added flyway queries

--- Back-End (Java-Spring)/DerpinesCards/db/migrations/V1_0_2__Product_Insertion_Candles.sql
+++ Back-End (Java-Spring)/DerpinesCards/db/migrations/V1_0_2__Product_Insertion_Candles.sql
@@ -6,6 +6,11 @@ INSERT INTO products
         ('LightCoral','2023-10-30T11:13:04.94569','Scented soy Candle, with Apple Cinnamon scent','https://i.postimg.cc/qvWsfS5L/image00002.jpg',8,'Apple Cinnamon Candle (280gr)','candle',280),
         ('SandyBrown','2023-10-30T11:13:04.94569','Scented soy Candle, with Pumpkin Pie scent','https://i.postimg.cc/Wp7wD9dN/image00003.jpg',9,'Pumpkin Pie Candle (200gr)','candle',200),
         ('Sienna','2023-10-30T11:13:04.94569','Scented soy Candle, with Pumpkin Pie scent','https://i.postimg.cc/Wp7wD9dN/image00003.jpg',9,'Pumpkin Pie Candle (200gr)','candle',200),
-        ('PowderBlue','2023-10-30T11:13:04.94569','Scented soy Candle, with fresh linen scent','https://i.postimg.cc/xC6V8vH6/image00001.jpg',8,'Fresh linen Candle (200gr)','candle',200),
-        ('PowderBlue','2023-10-30T11:13:04.94569','Scented soy Candle, with fresh linen scent','https://i.postimg.cc/bN3g56PC/image00005.jpg',10,'Woodland Candle (100ml)','candle',200),
-        ('White','2023-10-30T11:13:04.94569','Scented soy Candle, with fresh linen scent','https://i.postimg.cc/bN3g56PC/image00005.jpg',10,'Woodland Candle (100ml)','candle',200);
+        ('PowderBlue','2023-10-30T11:13:04.94569','Scented soy Candle, with fresh linen scent','https://i.postimg.cc/xC6V8vH6/image00001.jpg',8,'Fresh Linen Candle (200gr)','candle',200),
+        ('PowderBlue','2023-10-30T11:13:04.94569','Scented soy Candle, with apple cinamon scent','https://i.postimg.cc/bN3g56PC/image00005.jpg',10,'Apple Cinnamon Candle With Lid (190gr)','candle',200),
+        ('White','2023-10-30T11:13:04.94569','Scented soy Candle, with apple cinamon scent','https://i.postimg.cc/bN3g56PC/image00005.jpg',10,'Apple Cinnamon Candle With Lid (190gr)','candle',200),
+        ('White','2023-10-30T11:13:04.94569','Scented soy Candle, with macademia dream scent','https://i.postimg.cc/VLt4F2WX/image00022.jpg',10,'Macademia Dream Candle With Lid (550gr)','candle',200),
+        ('Purple','2023-10-30T11:13:04.94569','Scented soy Candle, with macademia dream scent','https://i.postimg.cc/VLt4F2WX/image00022.jpg',10,'Macademia Dream Candle With Lid (550gr)','candle',200),
+        ('SeaGreen','2023-10-30T11:13:04.94569','Scented soy Candle, with macademia dream scent','https://i.postimg.cc/VLt4F2WX/image00022.jpg',10,'Macademia Dream Candle With Lid (550gr)','candle',200),
+        ('Chocolate','2023-10-30T11:13:04.94569','Scented soy Candle, with carrot cake scent','https://i.postimg.cc/gj4Vf0CX/image00004.jpg',10,'Carrot Cake Candle (160gr)','candle',200),
+        ('PapayaWhip','2023-10-30T11:13:04.94569','Scented soy Candle, with carrot cake scent','https://i.postimg.cc/gj4Vf0CX/image00004.jpg',10,'Carrot Cake Candle (160gr)','candle',200);
---
+++ Back-End (Java-Spring)/DerpinesCards/db/migrations/V1_0_3__Product_Insertion_Others.sql
@@ -0,0 +1,18 @@
+INSERT INTO products
+        (colour,date_added,description,image,price,title,type,weight)
+    VALUES
+        ('Colorless','2023-10-30T11:13:04.94569','Scented room spray with fresh linen scent. Ideal for fabrics and curtains','https://i.postimg.cc/N0PpDXLz/image00001.jpg',6,'Fresh Linen Spray (100ml)','other',100),
+        ('BlueViolet','2023-10-30T11:13:04.94569','Scented tablet with fresh linen scent. Ideal for wardrobes and drawers','https://i.postimg.cc/K81PHxJj/image00006.jpg',5,'Wardrobe Freshner (40gr)','other',40),
+        ('CadetBlue','2023-10-30T11:13:04.94569','Scented tablet with fresh linen scent. Ideal for wardrobes and drawers','https://i.postimg.cc/K81PHxJj/image00006.jpg',5,'Wardrobe Freshner (40gr)','other',40),
+        ('Moccasin','2023-10-30T11:13:04.94569','Scented tablet with fresh linen scent. Ideal for wardrobes and drawers','https://i.postimg.cc/K81PHxJj/image00006.jpg',5,'Wardrobe Freshner (40gr)','other',40),
+        ('Beige','2023-10-30T11:13:04.94569','Scented wax melt toys with baby powder scent (14 items)','https://i.postimg.cc/G2CvtYrT/image00007.jpg',5,'Wax Melt Toys (20gr)','other',20),
+        ('DarkOliveGreen','2023-10-30T11:13:04.94569','Scented wax melt toys with baby powder scent (14 items)','https://i.postimg.cc/G2CvtYrT/image00007.jpg',5,'Wax Melt Toys (20gr)','other',20),
+        ('Salmon','2023-10-30T11:13:04.94569','Scented wax melt toys with baby powder scent (14 items)','https://i.postimg.cc/G2CvtYrT/image00007.jpg',5,'Wax Melt Toys (20gr)','other',20),
+        ('Gold','2023-10-30T11:13:04.94569','Scented wax melt toys with baby powder scent (14 items)','https://i.postimg.cc/G2CvtYrT/image00007.jpg',5,'Wax Melt Toys (20gr)','other',20),
+        ('Gold','2023-10-30T11:13:04.94569','Scented wax melt rainbows and clouds with cherry scent (10 items)','https://i.postimg.cc/j54P75dg/image00008.jpg',4.5,'Wax Melt Rainbow And Cloud (20gr)','other',20),
+        ('Salmon','2023-10-30T11:13:04.94569','Scented wax melt rainbows and clouds with cherry scent (10 items)','https://i.postimg.cc/j54P75dg/image00008.jpg',4.5,'Wax Melt Rainbow And Cloud (20gr)','other',20),
+        ('Colorless','2023-10-30T11:13:04.94569','Reed diffuser with woodland scent','https://i.postimg.cc/d0Dx6bTj/image00009.jpg',7,'Reed Diffuser (150ml)','other',150),
+        ('White','2023-10-30T11:13:04.94569','Heart shaped wax melts with vanilla scent','https://i.postimg.cc/R0jXRNmv/image00010.jpg',7,'Heart Shaped Wax Melts (80gr)','other',80),
+        ('DarkBlue','2023-10-30T11:13:04.94569','Heart shaped wax melts with vanilla scent','https://i.postimg.cc/R0jXRNmv/image00010.jpg',7,'Heart Shaped Wax Melts (80gr)','other',80),
+        ('MediumPurple','2023-10-30T11:13:04.94569','Heart shaped wax melts with vanilla scent','https://i.postimg.cc/R0jXRNmv/image00010.jpg',7,'Heart Shaped Wax Melts (80gr)','other',80),
+        ('Maroon','2023-10-30T11:13:04.94569','Heart shaped wax melts with vanilla scent','https://i.postimg.cc/R0jXRNmv/image00010.jpg',7,'Heart Shaped Wax Melts (80gr)','other',80);
